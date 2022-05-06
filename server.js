1e5607d04b3448b81a5e0129220901b6f366e569
Srivignesh S <sri28vignesh@gmail.com>
2022-05-06 13:02:47 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 5000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('<h1 style="color:green;">Node Home App - version-10!!</h1> \n');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
